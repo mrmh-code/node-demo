dd6a33745f869bd5d126a3971ea97db93c2ad291
mehedi <mehasan.softbd@gmail.com>
2024-10-14 12:11:05 +0600

    testing purpose

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var http = require('http');
 
-// Create a server object
+//create a server object:
 http.createServer(function (req, res) {
-  res.write('A Monk in Cloud'); // Write a response to the client
-  res.end(); // End the response
-}).listen(3000); // The server object listens on port 3000
+  res.write('A Monk in Cloud'); //write a response to the client
+  res.end(); //end the response
+}).listen(80); //the server object listens on port 80
